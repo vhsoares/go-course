e85eee26e9822f3ecadc6af3b924566e266f4c7a
vhsoares <vitor.soares@jointecnologia.com.br>
2020-07-20 02:10:50 -0300

    observador de sites criado e iniciado, adicionado o sistema

--- requests/main.go
+++ requests/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	// ao se adicionar um repositório do github é necessário dar um goget
+	"github.com/fatih/color"
 )
 
 func main() {
@@ -31,8 +34,7 @@ func resolveCommand(command int) {
 	switch command {
 	case 1:
 		// o _ ( underline ) pode ser utilizado pra demostrar o desinteresse em uma das variáveis, assim não é necessário que eu trabalhe com a variável que eu não quero trabalhar
-		resp, _ := monitor("http://github.com/vhsoares")
-		fmt.Println(resp)
+		monitor("http://github.com/vhsoares")
 	case 2:
 		fmt.Println("Mostrando Log")
 	case 3:
@@ -46,6 +48,12 @@ func resolveCommand(command int) {
 
 func monitor(site string) (*http.Response, error) {
 	resp, err := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		color.Green("Site %s carregado com sucesso!", site)
+	} else {
+		color.Red("Não foi possível acessar este site")
+	}
 
 	if err != nil {
 		return nil, err
